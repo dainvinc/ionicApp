7d77504b2a34d1d95574660a798fcbe3f8aa451a
Vishal <vishal.koosuri1@marist.edu>
2017-11-20 20:37:21 +0000

    Added a mapview with location and zoom level

--- src/pages/directions/directions.ts
+++ src/pages/directions/directions.ts
@@ -1,52 +1,33 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
 
-declare var google;
+declare var google: any;
 
-@IonicPage()
 @Component({
-  selector: 'page-home',
+  selector: 'page-directions',
   templateUrl: 'directions.html'
 })
 export class DirectionsPage {
-
-  @ViewChild('map') mapElement: ElementRef;
+  @ViewChild('map') mapRef: ElementRef;
   map: any;
-  start = 'chicago, il';
-  end = 'chicago, il';
-  directionsService = new google.maps.DirectionsService;
-  directionsDisplay = new google.maps.DirectionsRenderer;
-
   constructor(public navCtrl: NavController) {
-
+    
   }
-
-  ionViewDidLoad(){
-    this.initMap();
+  
+  ionViewDidLoad() {
+    this.loadMap();
+    console.log("IS the map loaded?");
   }
-
-  initMap() {
-    this.map = new google.maps.Map(this.mapElement.nativeElement, {
-      zoom: 7,
-      center: {lat: 41.85, lng: -87.65}
-    });
-
-    this.directionsDisplay.setMap(this.map);
+  
+  loadMap() {
+    const location = new google.maps.LatLng(51.570531, -43.254125);
+    
+    const options = {
+      center: location,
+      zoom: 10
+    };
+    
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
   }
-
-  calculateAndDisplayRoute() {
-    this.directionsService.route({
-      origin: this.start,
-      destination: this.end,
-      travelMode: 'DRIVING'
-    }, (response, status) => {
-      if (status === 'OK') {
-        this.directionsDisplay.setDirections(response);
-      } else {
-        window.alert('Directions request failed due to ' + status);
-      }
-    });
-  }
-
+  
 }
