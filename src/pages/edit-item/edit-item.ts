35e8c7ed6f6f9942c496be4834cc47213e000456
Vishal <vishal.koosuri1@marist.edu>
2017-11-24 05:57:41 +0000

    Resolved the conflicts for the edit-item page

--- src/pages/edit-item/edit-item.ts
+++ src/pages/edit-item/edit-item.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
-<<<<<<< HEAD
-import { Subscription } from 'rxjs/Subscription';
-import { AddItem } from '../../models/add-item/add-item.interface';
-=======
 import { AddItem } from '../../models/add-item/add-item.interface';
 import { Subscription } from 'rxjs/Subscription';
->>>>>>> 5feca38c31545f699e689cfebe0236494cec8bac
 
 /**
  * Generated class for the EditItemPage page.
@@ -20,46 +15,27 @@ import { Subscription } from 'rxjs/Subscription';
   selector: 'page-edit-item',
   templateUrl: 'edit-item.html',
 })
-<<<<<<< HEAD
 export class EditItemPage {
   addItemSubscription: Subscription;
   itemListRef$: FirebaseObjectObservable<AddItem>;
   
   addItem = {} as AddItem;
 
-=======
-
-export class EditItemPage {
-
-  addItemSubscription: Subscription;
-  itemListRef$: FirebaseObjectObservable<AddItem>;
-  addItem = {} as AddItem;
-  
->>>>>>> 5feca38c31545f699e689cfebe0236494cec8bac
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     private database: AngularFireDatabase) {
-<<<<<<< HEAD
-=======
-      
->>>>>>> 5feca38c31545f699e689cfebe0236494cec8bac
+
       const addItemId = this.navParams.get('addItemId');
       console.log(addItemId);
       
       this.itemListRef$ = this.database.object(`home/${addItemId}`);
       
       this.addItemSubscription = this.itemListRef$.subscribe(
-<<<<<<< HEAD
         addItem => this.addItem = addItem
       );
   }
   
-=======
-        addItem => this.addItem = addItem);
-  }
-
->>>>>>> 5feca38c31545f699e689cfebe0236494cec8bac
   editItem(addItem: AddItem) {
     this.itemListRef$.update(addItem);
     
@@ -69,8 +45,5 @@ export class EditItemPage {
   ionViewWillLeave() {
     this.addItemSubscription.unsubscribe();
   }
-<<<<<<< HEAD
-=======
 
->>>>>>> 5feca38c31545f699e689cfebe0236494cec8bac
 }
